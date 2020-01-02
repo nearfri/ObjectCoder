d325d3c1cff2ca638e5ca6fdf70c4f570ec95e9a
Ukjeong Lee <nearfri@gmail.com>
2020-01-03 00:39:58 +0900

    Code refactoring

--- Sources/ObjectCoder/Decoder/SingleValueObjectDecodingContainer.swift
+++ Sources/ObjectCoder/Decoder/SingleValueObjectDecodingContainer.swift
@@ -16,62 +16,70 @@ internal struct SingleValueObjectDecodingContainer: SingleValueDecodingContainer
     }
     
     func decode(_ type: Bool.Type) throws -> Bool {
-        return try Bool(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Int.Type) throws -> Int {
-        return try Int(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Int8.Type) throws -> Int8 {
-        return try Int8(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Int16.Type) throws -> Int16 {
-        return try Int16(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Int32.Type) throws -> Int32 {
-        return try Int32(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Int64.Type) throws -> Int64 {
-        return try Int64(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: UInt.Type) throws -> UInt {
-        return try UInt(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: UInt8.Type) throws -> UInt8 {
-        return try UInt8(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: UInt16.Type) throws -> UInt16 {
-        return try UInt16(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: UInt32.Type) throws -> UInt32 {
-        return try UInt32(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: UInt64.Type) throws -> UInt64 {
-        return try UInt64(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Float.Type) throws -> Float {
-        return try Float(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: Double.Type) throws -> Double {
-        return try Double(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode(_ type: String.Type) throws -> String {
-        return try String(value: decoder.storage.topContainer, codingPath: decoder.codingPath)
+        return try decodeValue(type: type)
     }
     
     func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
         return try decoder.unbox(decoder.storage.topContainer, as: type)
+    }
+    
+    private func decodeValue<T: InitializableWithAny>(type: T.Type) throws -> T {
+        let value = decoder.storage.topContainer
+        if decoder.nilDecodingStrategy.isNilValue(value) {
+            throw Errors.valueNotFound(codingPath: decoder.codingPath, expectation: type)
+        }
+        return try type.init(value: value, codingPath: decoder.codingPath)
     }
 }
